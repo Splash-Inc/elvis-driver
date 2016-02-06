29336558a4cf9cbf99e1b669bdff936f493af179
scriptype <scriptyper@gmail.com>
2016-02-06 18:40:33 +0200

    Add tests for public getProfile

--- tests/public/get_profile.js
+++ tests/public/get_profile.js
@@ -2,4 +2,35 @@ module.exports = function (test, utils, Elvis) {
 
   'use strict'
 
+  test('public getProfile', t => {
+
+    utils
+        .shouldRequireLogin({
+          test: t,
+          promise: client => client.getProfile()
+        })
+        .then(client => {
+          client
+              .getProfile()
+              .then(profile => {
+
+                t.equal(profile.username, utils.username,
+                    'Profile has the correct username')
+
+                t.assert(Array.isArray(profile.authorities),
+                    'Profile object contains list of authorities')
+
+                t.assert(Array.isArray(profile.groups),
+                    'Profile object contains list of groups')
+
+                t.end()
+
+              })
+              .catch(utils.catchError(t))
+
+        })
+        .catch(utils.catchError(t))
+
+  })
+
 }
