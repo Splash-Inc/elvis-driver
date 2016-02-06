220133359152d9c54c427a946e9a461d04009372
scriptype <scriptyper@gmail.com>
2016-02-06 23:03:01 +0200

    Improve tests

--- tests/public/login.js
+++ tests/public/login.js
@@ -7,19 +7,32 @@ module.exports = function (test, utils, Elvis) {
     var client = Elvis.createClient(utils.server)
 
     client
-        .login(utils.username, utils.password)
+        .login(utils.username + 'abc', utils.password)
         .then(data => {
 
-          t.equal(typeof data, 'object',
-              'Returns an object')
-
-          t.equal(typeof data.sessionId, 'string',
-              'Returns a session ID')
-
-          t.end()
+          t.end('Should fail with wrong username')
 
         })
-        .catch(utils.catchError(t))
+        .catch(error => {
+
+          t.pass('Should fail with wrong username')
+
+          client
+              .login(utils.username, utils.password)
+              .then(data => {
+
+                t.equal(typeof data, 'object',
+                    'Returns an object')
+
+                t.equal(typeof data.sessionId, 'string',
+                    'Returns a session ID')
+
+                t.end()
+
+              })
+              .catch(utils.catchError(t))
+
+        })
   })
 
 }
