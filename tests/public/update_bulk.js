4e49d60973f125889ac4614f54d8ba84f8fb05f1
scriptype <scriptyper@gmail.com>
2016-02-07 16:01:55 +0200

    Add tests for public updateBulk

--- tests/public/update_bulk.js
+++ tests/public/update_bulk.js
@@ -1,5 +1,74 @@
 module.exports = function (test, utils, Elvis) {
 
-  'use strict'
+  test('public updateBulk', t => {
+
+    utils
+        .shouldRequireLogin(t, client => client.updateBulk())
+        .then(client => {
+
+          var timestamp = new Date().getTime()
+          var _assets = [
+            {
+              name: `foo-${timestamp}.txt`,
+              path: `${utils.folderPath}/foo-${timestamp}.txt`
+            },
+            {
+              name: `bar-${timestamp}.txt`,
+              path: `${utils.folderPath}/bar-${timestamp}.txt`
+            },
+            {
+              name: `bam-${timestamp}.txt`,
+              path: `${utils.folderPath}/bam-${timestamp}.txt`
+            }
+          ]
+
+          // Create 3 assets to bulk-update later on
+          Promise
+              .all(_assets.map(asset => client.create({ assetPath: asset.path })))
+              .then(() => {
+
+                // Bulk-update
+                client
+                    .updateBulk({
+                      q: timestamp,
+                      cf_follow: 'lorem'
+                    })
+                    .then(() => {
+
+                      client
+                          .search({ q: timestamp })
+                          .then(results => {
+
+                            var bulkUpdatedAssets = results.hits.filter(hit => (
+                                hit.metadata.name.match(timestamp) &&
+                                hit.metadata.cf_follow.indexOf('lorem') > -1
+                            ))
+
+                            t.equal(bulkUpdatedAssets.length, 3,
+                                'There should be 3 bulk-updated assets')
+
+                            _assets.forEach(asset => {
+                              var bulkUpdatedAsset = bulkUpdatedAssets.filter(a => (
+                                  a.metadata.name === asset.name
+                              ))[0]
+
+                              t.assert(bulkUpdatedAsset, 'Asset bulk-updated')
+                            })
+
+                            t.end()
+
+                          })
+                          .catch(utils.catchError(t))
+
+                    })
+                    .catch(utils.catchError(t))
+
+              })
+              .catch(utils.catchError(t))
+
+        })
+        .catch(utils.catchError(t))
+
+  })
 
 }
