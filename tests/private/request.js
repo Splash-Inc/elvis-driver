12da053e7cc5218cea1c13915d00ae50d9e3a726
scriptype <scriptyper@gmail.com>
2016-02-16 16:19:58 +0200

    Remove unnecessary parts from tests of private request

--- tests/private/request.js
+++ tests/private/request.js
@@ -6,51 +6,41 @@ module.exports = function (test, utils, Elvis) {
 
     var client = Elvis.createClient(utils.server)
 
-    client.__request({
-      url: client.__getRemoteURL('/services/search'),
-      params: { q: '' },
-      success() { t.end('Should fail when not logged in') },
-      failure() {
-        t.pass('Should fail when not logged in')
+    client
+        .login({
+          username: utils.username,
+          password: utils.password
+        })
+        .then(() => {
 
-        client
-            .login({
-              username: utils.username,
-              password: utils.password
-            })
-            .then(() => {
+          client.__request({
+            url: client.__getRemoteURL('/services/search'),
+            params: { q: '' },
+            failure: utils.catchError(t),
+            success: data => {
+
+              t.equal(typeof data, 'object',
+                  'Responded an object')
+
+              t.equal(typeof data.totalHits, 'number',
+                  'Returned object has the totalHits field')
+    
+              var timestamp = utils.getUniqueName()
 
               client.__request({
-                url: client.__getRemoteURL('/services/search'),
-                params: { q: '' },
-                failure: utils.catchError(t),
-                success: data => {
-
-                  t.equal(typeof data, 'object',
-                      'Responded an object')
-
-                  t.equal(typeof data.totalHits, 'number',
-                      'Returned object has the totalHits field')
-
-                  var timestamp = new Date().getTime()
-
-                  client.__request({
-                    url: client.__getRemoteURL(`/lorem/ipsum/${timestamp}`),
-                    success() { t.end('Should fail with wrong url') },
-                    failure() {
-                      t.pass('Should fail with wrong url')
-                      t.end()
-                    }
-                  })
-
+                url: client.__getRemoteURL(`/lorem/ipsum/${timestamp}`),
+                success() { t.end('Should fail with wrong url') },
+                failure() {
+                  t.pass('Should fail with wrong url')
+                  t.end()
                 }
               })
 
-            })
-            .catch(utils.catchError(t))
+            }
+          })
 
-      }
-    })
+        })
+        .catch(utils.catchError(t))
   })
 
 }
