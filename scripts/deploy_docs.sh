b0ca7ffa2bc5543687478234f8afd32cfc43c38e
scriptype <scriptyper@gmail.com>
2016-02-16 19:07:48 +0200

    Add deploy-docs script

---
+++ scripts/deploy_docs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cd docs
+
+git init
+
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.org"
+
+git add .
+git commit -m "Generate docs and deploy to gh-pages"
+
+git push --force --quiet "https://${GH_TOKEN}@github.com/Splash-Inc/elvis-driver.git" HEAD:gh-pages > /dev/null
