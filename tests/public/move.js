39d00b6f618e30d08b181d2dd64c48c8451a6d6c
scriptype <scriptyper@gmail.com>
2016-05-23 14:59:44 +0300

    Add the basic test case for move method

---
+++ tests/public/move.js
@@ -0,0 +1,71 @@
+
+var fs = require('fs')
+
+module.exports = function (test, utils, Elvis, isBrowser) {
+
+  'use strict'
+
+  test('public move', t => {
+
+    utils
+        .shouldRequireLogin(t, client => client.move(), Elvis)
+        .then(client => {
+
+          var timestamp = utils.getUniqueName()
+          var _asset = {
+            name: `foo-to-be-moved-${timestamp}.txt`,
+            rename: `foo-renamed-${timestamp}.txt`,
+            path: `${utils.folderPath}/foo-to-be-moved-${timestamp}.txt`
+          }
+
+          var tests = [
+
+            // Create asset
+            client
+              .create({ assetPath: _asset.path })
+              .then(file => {
+                
+                // Move asset
+                return client
+                  .move({
+                    source: file.metadata.assetPath,
+                    target: `${file.metadata.folderPath}/${_asset.rename}`
+                  })
+                  .then(() => {
+
+                    // Search for old asset
+                    return client
+                      .search({ q: _asset.name })
+                      .then(results => {
+                        var foundFile = results.hits.filter(hit => (
+                            hit.metadata.name === _asset.name &&
+                            hit.metadata.id === file.metadata.id
+                        ))[0]
+
+                        t.false(foundFile, 'Old file has gone')
+
+                          return client
+                            .search({ q: _asset.rename })
+                            .then(results => {
+                              var foundFile = results.hits.filter(hit => (
+                                  hit.metadata.name === _asset.rename
+                              ))[0]
+                              t.assert(foundFile, 'Found relocated file')
+                            })
+                      })
+                  })
+              })
+
+          ]
+
+          Promise.all(tests).then(() => {
+            t.end()
+          })
+          .catch(utils.catchError(t))
+
+        })
+        .catch(utils.catchError(t))
+
+  })
+
+}
